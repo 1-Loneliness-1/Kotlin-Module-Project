3844222c070aca60e6bc1f843819927288a580d0
Loneliness <costya.kisurin@yandex.ru>
2024-01-05 11:51:00 +0300

    Removed redundant app screen to be more in line with the job. The behavior of ArchiveActivity class was adapted to the changes.

--- src/main/kotlin/ArchiveActivity.kt
+++ src/main/kotlin/ArchiveActivity.kt
@@ -1,22 +1,31 @@
 import Menu.Companion.getUserInput
+import OpenArchiveActivity.Companion.startOpenArchiveActivity
 
 class ArchiveActivity {
 
-    private val archiveMenuElements: ArrayList<String> = arrayListOf(
-        "Создать архив",
-        "Выбрать уже созданный архив",
-        "Выйти из программы"
-    )
-    private val firstLineInOutput = "Действия с архивами:"
+    private var firstLineInOutput = "Действия с архивами:"
 
     fun start() {
         while(true) {
-            when (getUserInput(firstLineInOutput, archiveMenuElements)) {
+            val namesOfCurrentArchives: List<String> = currentArchives.keys.toList()
+            val archiveMenuElements: ArrayList<String> = arrayListOf(
+                "Создать архив",
+                "Выйти из программы"
+            )
+
+            namesOfCurrentArchives.forEach {
+                archiveMenuElements.add("Открыть архив \'$it\'")
+            }
+            firstLineInOutput = if (currentArchives.isEmpty()) firstLineInOutput.plus("\nАрхивы не созданы!") else "Действия с архивами:"
+
+            when (val command = getUserInput(firstLineInOutput, archiveMenuElements)) {
                 "0" -> goToCreateActivity()
-                "1" -> ShowArchiveActivity().startShowArchiveActivity()
-                "2" -> {
+                "1" -> {
                     exit()
                     break
+                }
+                else -> {
+                    startOpenArchiveActivity(namesOfCurrentArchives[command.toInt() - 2])
                 }
             }
         }
